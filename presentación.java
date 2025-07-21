bf0aab3b55f91d073dba29ac145eece0d10e80b9
Hanna-art543 <amirandalo@unsa.edu.pe>
2025-07-20 22:18:17 -0500

    presentación_código.java

---
+++ presentación.java
@@ -0,0 +1,10 @@
+public class presentación {
+    public static void main(String[] args) {
+        System.out.println("Tengo 17 años.");
+        System.out.println("Vivo en Arequipa, Paucarpata.");
+        System.out.println("Me gusta el ceviche y el color rojo.");
+        System.out.println("Disfruto ver anime con mi familia.");
+        System.out.println("Me agrada el fútbol; cuando era niña lo practicaba.");
+        System.out.println("En mi tiempo libre me gusta escuchar música, dibujar y leer.");
+    }
+}
